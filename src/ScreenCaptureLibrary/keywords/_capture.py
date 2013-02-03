574890cf6b21a765c787ac5ac03198d35219c049
Ed Manlove <devPyPlTw@verizon.net>
2013-02-02 19:05:36 -0500

    Starting core of take screenshot keyword and parser.

--- src/ScreenCaptureLibrary/keywords/_capture.py
+++ src/ScreenCaptureLibrary/keywords/_capture.py
@@ -1,10 +1,31 @@
-class CaptureKeywords(object):
+from robot.api import logger
+
+AREA_DEFINING_ACTIONS = ['of', 'from', 'down', 'right', 'up', 'left']
+DIRECTIONAL_MARKERS = ['of', 'past', 'to']
+
+class _CaptureKeywords(object):
 
     def take_screenshot(self, *args):
         """
+        Example:
+        | Take screenshot | of id=header|
+        
+        To do:
+        | Take screenshot | of id=header|
+        | ... | from <element> down|right|up|left [of/to <element>]
+        | ... | add <drawing object>
+	| ... | obscuring <element>
+	| ... | (saving) as
+        
         """
-
-        print "Hello World"
+        #import pdb, sys; pdb.Pdb(stdout=sys.__stdout__).set_trace()
+        # area starts with full screen
+        sel2lib = self._sel2lib
+        
+        body = sel2lib._element_find('tag=body',True, True)
+        area = body.size
+        for arg in args:
+            self._parse_arg(arg)
         
     def capture_region(self):
         """
@@ -24,4 +45,17 @@ class CaptureKeywords(object):
 
         pass
     
-    
+    def _parse_arg(self, arg):
+        """
+        Highly simple parser to parse out action ('of', 'from', ... )
+        from noun (<element> or <screenarea>)
+        """
+        noun = arg
+        while noun:
+            action, sep, noun = noun.partition(' ')
+            print action, sep, noun
+            logger.console("\n%s :: %s :: %s" % (action, sep, noun))
+
+    def _define_area(self):
+        pass
+
